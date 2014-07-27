6d017db54c392c81eac4ef7415686745e24bbd6c
Robert Niederreiter <office@squarewave.at>
2014-07-27 15:33:41 +0200

    stub

---
+++ src/yafowil/widget/recaptcha/widget.py
@@ -0,0 +1,39 @@
+from node.utils import UNSET
+from yafowil.common import generic_required_extractor
+
+
+def recaptcha_extractor(widget, data):
+    return UNSET
+
+
+@managedprops(*css_managed_props)
+def recaptcha_edit_renderer(widget, data):
+    return 
+
+
+def recaptcha_display_renderer(widget, data):
+    pass
+
+
+factory.register(
+    'recaptcha',
+    extractors=[recaptcha_extractor, generic_required_extractor],
+    edit_renderers=[recaptcha_edit_renderer],
+    display_renderers=[recaptcha_display_renderer])
+
+factory.doc['blueprint']['recaptcha'] = \
+"""Add-on blueprint
+`yafowil.widget.recaptcha <http://github.com/bluedynamics/yafowil.widget.recaptcha/>`_
+"""
+
+factory.defaults['recaptcha.class'] = 'recaptcha'
+
+factory.defaults['recaptcha.required'] = True
+
+factory.defaults['recaptcha.error_class'] = 'error'
+
+factory.defaults['recaptcha.message_class'] = 'errormessage'
+
+#factory.defaults['recaptcha.'] = 47.2667
+#factory.doc['props']['recaptcha.'] = """\
+#"""
---
+++ src/yafowil/widget/recaptcha/widget.rst
@@ -0,0 +1,9 @@
+Import requirements::
+
+    >>> import yafowil.loader
+    >>> from yafowil.base import factory
+
+Render map widget with defaults::
+
+    >>> widget = factory('recaptcha', 'recaptcha')
+    >> pxml(widget())
