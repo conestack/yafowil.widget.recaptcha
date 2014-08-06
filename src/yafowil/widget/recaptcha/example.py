f5f317515444165cf4aea32d9959d82f2262f34a
Robert Niederreiter <office@squarewave.at>
2014-08-06 13:58:57 +0200

    ...

--- src/yafowil/widget/recaptcha/example.py
+++ src/yafowil/widget/recaptcha/example.py
@@ -1,2 +1,47 @@
+import os
+from yafowil.base import factory
+
+
+DOC_RECAPTCHA = """
+reCAPTCHA
+---------
+
+reCAPTCHA widget.
+
+.. code-block:: python
+
+    recaptcha_public_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    recaptcha_private_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    captcha = factory('#field:recaptcha', props={
+        'label': 'Verify',
+        'public_key': recaptcha_public_key,
+        'private_key': recaptcha_private_key,
+        'lang': 'de',
+        'theme': 'clean',
+        'error.position': 'after',
+    })
+"""
+
+def recaptcha_example():
+    form = factory('fieldset', name='yafowil.widget.recaptcha.recaptcha')
+    recaptcha_public_key = os.environ['recaptcha-public-key']
+    recaptcha_private_key = os.environ['recaptcha-private-key']
+    form['captcha'] = factory('#field:recaptcha', props={
+        'label': 'Verify',
+        'public_key': recaptcha_public_key,
+        'private_key': recaptcha_private_key,
+        'lang': 'de',
+        'theme': 'clean',
+        'error.position': 'after',
+    })
+    return {
+        'widget': form,
+        'doc': DOC_RECAPTCHA,
+        'title': 'reCAPTCHA',
+    }
+
+
 def get_example():
-    return []
+    return [
+        recaptcha_example(),
+    ]
