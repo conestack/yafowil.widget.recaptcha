57f276183568c91e7edc2988770f00af790d4999
Georg Gogo. Bernhard <gogo@bluedynamics.com>
2018-06-13 12:38:12 +0200

    Python3 compatibility WIP

--- src/yafowil/widget/recaptcha/tests.py
+++ src/yafowil/widget/recaptcha/tests.py
@@ -1,3 +1,8 @@
+from yafowil.compat import IS_PY2
+
+if not IS_PY2:
+    from importlib import reload 
+
 from node.tests import patch
 from node.utils import UNSET
 from yafowil.base import ExtractionError
